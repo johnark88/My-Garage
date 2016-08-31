b22337e729c1f09482dcf7405efdfc121211f376
John Arkema <john.arkema@gmail.com>
2016-08-31 14:04:17 -0500

    More jQuery

--- jqGarage.js
+++ jqGarage.js
@@ -13,31 +13,35 @@ $( document ).on('click' , '#addCar' ,function (){
 };//end New Car
 if(newCar.year == ''|| newCar.make == ''|| newCar.model == ' ' ){
   alert('Please fill in the required fields');
-}else{
-$('#carInput > input').each( function () {
+}else
+{
+  $('#carInput > input').each( function () {
   $(this).val('');
 });
-garage.push(newCar);
-// displayCars();
-console.log(garage);
-displayCars();
+  garage.push(newCar);
+  // displayCars();
+  console.log(garage);
+  displayCars();
 }
 });
-$('#hideInputs').on('click', function (){
-  $('#hideInputs').slideToggle();
-});
-
 
 var displayCars = function() {
   console.log('in displayCars');
   console.log(garage);
 $('#allCars').empty();
-for (var i = 0; i < garage.length; i++) {
-  // car bold text
+  for (var i = 0; i < garage.length; i++) {
+    // car bold text
     $( '#allCars' ).append( '<p><b>' + garage[ i ].year + ' ' + garage[ i ].make + ' ' + garage[ i ].model + '</b></p>' );
     // car description
     $( '#allCars' ).append( '<p>' + garage[ i ].description + '</p>' );
     // car image
     $( '#allCars' ).append( '<img src="' + garage[ i ].picURL + '">' );
+
   } // end for
 };//end displayCars
+
+$(document).ready(function(){
+  $('#hideInputs').click(function (){
+    $('#carInput').slideToggle();
+  });
+  });
